3bc530140d856a5f226d6b48ac2639aa3c1b51b1
Monsieur Tino <contact@mrtino.eu>
2016-02-24 20:54:02 +0100

    Deux versions sont désormais disponibles : une pour Python 2 et l'autre pour Python 3.

---
+++ puissance_python3.py
@@ -0,0 +1,14 @@
+from math import pow
+# -*- coding: utf-8 -*-
+x = 5
+y = 4
+
+bienvenue = input("Bienvenue dans le calculateur de puissances ! Appuie sur Entrée.")
+
+def pow():
+    x = int(input("Entre la base : "))
+    y = int(input("Entre l\'exposant : "))
+    resultat = x**y
+    print("%s à la puissance %s vaut %s." % (x, y, resultat))
+
+pow()
